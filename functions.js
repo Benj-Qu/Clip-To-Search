d6e3acbe3781a5eda2c0292cfaf4da7edfb5b3db
NothingButBugs <strong_ji@outlook.com>
2022-05-26 15:44:18 -0400

    commit

--- functions.js
+++ functions.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
     function inBox(selector, x1, y1, x2, y2){
-        var elements = [];
+        var objects = [];
         $(selector).each(function(){
             var offSet = $(this).offset;
             var x = offSet.left;
             var y = offSet.top;
             var w = $(this).width;
             var h = $(this).height;
-            if (x >= x1 
-                && y >= y1 
-                && x + w <= x2 
-                && y + h <= y2) {
-                elements.push($this.get(0));
+            if (x >= x1 && y >= y1 && x + w <= x2 && y + h <= y2) {
+                objects.push($this.get(0));
             }
         });
-        return elements;
+        return objects;
     }
 });
