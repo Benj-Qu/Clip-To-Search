8170a6df3956c0002391d9b0ca313f73d35694be
NothingButBugs <sizhuang@umich.edu>
2022-05-26 17:03:51 -0400

    minor changes

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
-$( document ).on( "mousedown", function( event ) {
+$( document ).on( "mousedown", function(event){
     var mouseDownX = event.pageX;
     var mouseDownY = event.pageY;
 });
 
-$( document ).on( "mouseup", function( event ) {
+$( document ).on( "mouseup", function(event){
     var mouseUpX = event.pageX;
     var mouseUpY = event.pageY;
 });
