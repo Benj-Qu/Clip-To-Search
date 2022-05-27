c165def7ce275fd9e10fc25c5046e1c0ad47ebd0
NothingButBugs <sizhuang@umich.edu>
2022-05-27 11:00:43 -0400

    fixed everything up till rectangleSelect

--- content.js
+++ content.js
@@ -1,28 +1,31 @@
 
 console.log("Clip to Search, Start");
+var mouseDownX = 0;
+var mouseDownY = 0;
+var mouseUpX = 0;
+var mouseUpY = 0;
 
 $(document).ready(function(){
-    var mouseDownX, mouseDownY, mouseUpX, mouseUpY;
-
+    
     $(document).on("mousedown", function(event){
+
         mouseDownX = event.pageX;
         mouseDownY = event.pageY;
-        
-    });
-    
-    $(document).on("mouseup", function(event){
-        mouseUpX = event.pageX;
-        mouseUpY = event.pageY;
-    
-    });
-    
-    console.log(mouseDownX);
-    
-    var objects = [];
-    objects = rectangleSelect(mouseDownX, mouseDownY, mouseUpX, mouseUpY);
-    
-    
-    // for(elt in objects){
-    //     console.log(elt.html())
-    // }
+
+        $(document).on("mouseup", function(event){
+            mouseUpX = event.pageX;
+            mouseUpY = event.pageY;
+
+            console.log("downX: ", mouseDownX, ", downY: ", mouseDownY, ", upX: ", mouseUpX, ", upY: ", mouseUpY);        
+            
+            var objects = [];
+            objects = rectangleSelect(mouseDownX, mouseDownY, mouseUpX, mouseUpY);
+            
+            console.log("length: ", objects.length)
+            
+            for(elt in objects){
+                console.log(elt.html())
+            }
+        });
+    });  
 });
