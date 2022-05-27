e03884fb03f65cd3abeee2bb8eb15b0a5e6e6ee3
NothingButBugs <sizhuang@umich.edu>
2022-05-26 23:44:43 -0400

    added jQuery and tested fine but one major problem is how to pass in the arguments to the event functions

--- content.js
+++ content.js
@@ -1,12 +1,28 @@
-$( document ).on( "mousedown", function(event){
-    var mouseDownX = event.pageX;
-    var mouseDownY = event.pageY;
-});
 
-$( document ).on( "mouseup", function(event){
-    var mouseUpX = event.pageX;
-    var mouseUpY = event.pageY;
-});
+console.log("Clip to Search, Start");
 
-var objects = [];
-objects = rectangleSelect(mouseDownX, mouseDownY, mouseUpX, mouseUpY);
+$(document).ready(function(){
+    var mouseDownX, mouseDownY, mouseUpX, mouseUpY;
+
+    $(document).on("mousedown", function(event){
+        mouseDownX = event.pageX;
+        mouseDownY = event.pageY;
+        
+    });
+    
+    $(document).on("mouseup", function(event){
+        mouseUpX = event.pageX;
+        mouseUpY = event.pageY;
+    
+    });
+    
+    console.log(mouseDownX);
+    
+    var objects = [];
+    objects = rectangleSelect(mouseDownX, mouseDownY, mouseUpX, mouseUpY);
+    
+    
+    // for(elt in objects){
+    //     console.log(elt.html())
+    // }
+});
