f43c1c006f8ae1471ad8c0a5bb69de480a1f5af5
Benjamin Qu <quzhemin@umich.edu>
2022-06-09 02:35:41 -0400

    QAQQQ

--- background.js
+++ background.js
@@ -3,17 +3,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(
         tab.id, 
         { file: "rectangle.js" }, 
-        function() {
-            if (chrome.runtime.lastError) {
-                alert(chrome.i18n.getMessage("launch_error"));
-            }
-        }
     );
     chrome.tabs.insertCSS(
         tab.id, 
         { file: "rectangle.css" }, 
-        function() { 
-            if (chrome.runtime.lastError) { } 
-        }
     );
 });
