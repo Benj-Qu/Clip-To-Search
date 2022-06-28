a03a00d32d22fcd6b64e6113e71199090020700f
Benj-Qu <quzhemin@umich.com>
2022-06-28 11:56:51 -0400

    QAQ

---
+++ helper.js
@@ -0,0 +1,106 @@
+// Disables text selection on the current page
+function disableTextSelection(){
+    document.querySelector('*').addEventListener('selectstart', (e) => {
+        e.preventDefault();
+    });
+}
+// Get the coordinates of "element"
+function getPos(element) {
+    let rect = element.getBoundingClientRect();
+    let x_l = rect.left + parseInt(getComputedStyle(element).paddingLeft),
+        x_r = rect.right - parseInt(getComputedStyle(element).paddingRight),
+        y_t = rect.top + parseInt(getComputedStyle(element).paddingTop),
+        y_d = rect.bottom - parseInt(getComputedStyle(element).paddingBottom);
+    let pos = [x_l, x_r, y_t, y_d];
+    return pos;
+}
+
+function checkExisted(element, elementList) {
+    for (const ele of elementList) {
+        if (ele == element) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+// In: searchList is an array whose elements are arrays in this form: [DOM object, x_origin, y_origin]
+// You are supposed to pass in this.objectToSearch
+// Out: 
+function searchelement(searchList){
+    // Result of seasrching by each element
+    // 2D array
+    let singleSearchResult = [];
+
+    // Initialization of SSR
+    for(var i = 0; i < searchList.length; i++) {
+        let emptyList = [];
+        singleSearchResult.push(emptyList);
+        // singleSearchResult looks like [[], [], [], [], [], [], [], []]
+    }
+
+    // Fill in the 2D array, each element with ele([0]) and parent([1])
+    [...document.querySelectorAll("*")].forEach((ele)=>{
+        for (var i = 0; i < searchList.length; i++) {
+            //console.log(ele.outerHTML);
+            if (ele.outerHTML == searchList[i].outerHTML) {
+                singleSearchResult[i].push([ele,-1]);
+                // parent init as -1
+            }
+        }
+    });
+
+    // top-down
+    for (var i = 0; i < singleSearchResult.length - 1; i++) {
+        for (var j = 0; j < singleSearchResult[i].length; j++) {
+            if ((i == 0) || (singleSearchResult[i][j][1] != -1)) {
+                let pos = getPos(singleSearchResult[i][j][0]);
+                let x_l = pos[0],
+                    y_t = pos[2];
+                let searchPos = getPos(searchList[i]),
+                    _searchPos = getPos(searchList[i+1]);
+                let x_dist = _searchPos[0] - searchPos[0],
+                    y_dist = _searchPos[2] - searchPos[2];
+                for (var k = 0; k < singleSearchResult[i+1].length; k++) {
+                    let _pos = getPos(singleSearchResult[i+1][k][0]);
+                    let _x_l = _pos[0],
+                        _y_t = _pos[2];
+                    if ((_x_l - x_l == x_dist) && 
+                        (_y_t - y_t == y_dist)) {
+                            singleSearchResult[i+1][k][1] = j;
+                        }
+                }
+            }
+        }
+    }
+
+    // bottom-up
+    if (singleSearchResult.length == 1) {
+        for (var j = 0; j < singleSearchResult[0].length; j++) {
+            singleSearchResult[0][j][0].classList.add("mystyle");
+        }
+    } else if (singleSearchResult.length > 1) {
+        for (var i = 0; i < singleSearchResult[singleSearchResult.length - 1].length; i++) {
+            if (singleSearchResult[singleSearchResult.length - 1][i][1] != -1) {
+                var idx = i;
+                for (var j = singleSearchResult.length - 1; j >= 0; j--) {
+                    singleSearchResult[j][idx][0].classList.add("mystyle");
+                    idx = singleSearchResult[j][idx][1];
+                }
+            }
+        }
+    }
+}
+   
+function getView(view) {
+    let html;
+    $.ajax({
+      url: view,
+      async: false,
+      success: function(data) {
+        html = $.parseHTML(data.response);
+      }
+    });
+    return html;
+}
--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,93 @@
 class Sidebar {
     constructor () {
+        innerhtml = $("<div id='sidebar'></div>");
+    }
+
+    sidebarInit() {
+        $('body').css({
+            'padding-right': '600px'
+        });
+        this.sidebar.css({
+            'all': 'initial',
+            'position': 'fixed',
+            'right': '0px',
+            'top': '0px',
+            'z-index': 9999,
+            'width': '500px',
+            'height': '100%',
+            'overflow-y': 'scroll',
+            'background-color': 'GhostWhite',
+        });
+        $('body').append(this.sidebar);
+        let title = $('<h1>Sidebar</h1>');
+        title.css({
+            'font-size': '60px',
+            'font-weight': 'bold',
+        });
+        let repo_head = $('<h1>Repository</h1>');
+        repo_head.css({
+            'font-size': '40=30px',
+            'font-weight': 'bold',
+        });
+        let repo = $('<ul id="repo"></ul>');
+        repo_head.append($('<hr class="solid">'));
+        repo_head.append(repo);
+        this.sidebar.append(title);
+        this.sidebar.append($('<hr class="solid">'));
+        this.sidebar.append(repo_head);
+    }
+
+    appendToSidebar(html){
+        console.log("append");
+        let li = $('<li></li>');
+        li.append(html);
+
+        let btn_group = $('<div></div>');
+        btn_group.css({
+            'display': 'inline-block',
+            'vertical-align': 'middle',
+        });
+        let raw_btn = $('<button>Raw</button>');
+        let rendered_btn = $('<button>Rendered</button>');
+        raw_btn.css({
+            'border': '1px solid black',
+            'padding': '10px',
+        });
+        rendered_btn.css({
+            'border': '1px solid black',
+            'padding': '10px',
+        });
+        btn_group.append(raw_btn); 
+        li.append(btn_group);
+        $("#repo").append(li);
+        $("#repo").append($('<hr class="dashed">'));
         
+        let raw_html = html.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+        var new_p = $('<p />').append(raw_html);
+        
+        raw_btn.click(function(){
+            console.log('raw_btn clicked');
+            li.replaceWith(function(){
+                new_p.append(rendered_btn);
+                return new_p;
+            });
+        });
+        rendered_btn.click(function(){
+            console.log('rendered_btn clicked');
+            new_p.replaceWith(li);
+        });
+        
+        //console.log(div);
+    }
+    
+    clearSiderbar(){
+        console.log("clear sidebar");
+        this.sidebar.empty();
+        let title = $('<p>Sidebar</p>');
+        title.css({
+            'font-size': '60px',
+            'font-weight': 'bold',
+        });
+        this.sidebar.append(title);
     }
 }
